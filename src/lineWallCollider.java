85386a0470360d793d506deab0dffd687ab91330
Mahbod <mahbodsabbaghi@gmail.com>
2019-06-08 16:45:03 -0400

    -Added line walla collider

---
+++ src/lineWallCollider.java
@@ -0,0 +1,19 @@
+import processing.core.PApplet;
+import processing.core.PVector;
+
+public class lineWallCollider {
+    private PVector a, b;
+    private PApplet parent;
+    public lineWallCollider(int x1, int x2, int y1, int y2, PApplet applet){
+        parent = applet;
+        a = new PVector(x1,y1);
+        b = new PVector(x2,y2);
+    }
+    public void showBoundary(){
+        parent.stroke(255);
+        parent.line(a.x, a.y, b.x, b.y);
+    }
+//    public void testMessage(){
+//        System.out.println("Test Print!");
+//    }
+}
